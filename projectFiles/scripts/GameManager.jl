6a23aefab764ee1fc6641fc1d37ac5b891e77f4f
Kyle Conel <kyjordev@gmail.com>
2023-08-17 05:28:14 -0400

    Moving requests into game manager

--- projectFiles/scripts/GameManager.jl
+++ projectFiles/scripts/GameManager.jl
@@ -1,10 +1,23 @@
 using JulGame.MainLoop 
+using Firebase
 
 mutable struct GameManager
     parent
+    playerId
+    results
+    roomState
+    task
+    tickRate
+    tickTimer
+    user 
 
     function GameManager()
         this = new()
+
+        this.roomState = C_NULL
+        this.tickRate = 2
+        this.tickTimer = 0.0
+        this.task = C_NULL
 
         return this
     end
@@ -13,14 +26,52 @@ end
 function Base.getproperty(this::GameManager, s::Symbol)
     if s == :initialize
         function()
+            Firebase.realdb_init("https://multiplayer-demo-2f287-default-rtdb.firebaseio.com")
+            Firebase.set_webapikey("AIzaSyCxuzQNfmIMijosSYn8UWfQGOrQYARJ4iE")
+            this.user = Firebase.firebase_signinanon()
+            initialPlayerState = Dict("id" => this.user["localId"], "name" => "toto", "direction" => "right", "color" => "blue", "x" => 3, "y" => 3, "coins" => 0)
+            this.playerId = Firebase.realdb_postRealTime("/players/$(this.user["localId"])",initialPlayerState, this.user["idToken"])["name"]
             MAIN.cameraBackgroundColor = [252, 223, 205]
         end
     elseif s == :update
         function(deltaTime)
+            if this.task != C_NULL # Not sure why this is needed, but it doesn't work without it
+                try
+                    print("")
+                catch e
+                    this.task = C_NULL
+                    println(e)
+                    Base.show_backtrace(stderr, catch_backtrace())
+                end
+            end
+            if this.roomState != C_NULL
+                println(this.roomState)
+                this.roomState = C_NULL
+            end
+            this.tickTimer += deltaTime
+            if this.tickTimer >= 1/this.tickRate
+                this.task = Threads.@spawn this.get()
+                this.tickTimer = 0.0
+            end
         end
     elseif s == :setParent 
         function(parent)
             this.parent = parent
+        end
+    elseif s == :get
+        function ()
+            res = nothing
+            try
+                res = Firebase.realdb_getRealTime("/players", this.user["idToken"])
+            catch e
+                print(e)
+            end
+            this.roomState = res
+        end
+    elseif s == :onShutDown
+        function ()
+            println("shut down")
+            Firebase.realdb_deleteRealTime("/players/$(this.user["localId"])", this.user["idToken"])
         end
     else
         try
--- projectFiles/scripts/PlayerMovement.jl
+++ projectFiles/scripts/PlayerMovement.jl
@@ -1,23 +1,17 @@
 using JulGame.Macros
 using JulGame.MainLoop
 using JulGame.SoundSourceModule
-using Firebase
-using HTTP
 
-# Firebase.realdb_deleteRealTime("/players/$(user["localId"])", user["idToken"])
 mutable struct PlayerMovement
     animator
     canMove
+    gameManager
     input
     isFacingRight
     isJump 
     jumpSound
     parent
-    playerId
-    user 
-    test 
-    results
-    roomState
+
 
     function PlayerMovement()
         this = new()
@@ -28,9 +22,7 @@ mutable struct PlayerMovement
         this.isJump = false
         this.parent = C_NULL
         this.jumpSound = SoundSourceModule.SoundSource(joinpath(pwd(),"..",".."), "Jump.wav", 1, 50)
-        this.test = C_NULL
-        this.results = Channel()
-        this.roomState = C_NULL
+        this.gameManager = MAIN.scene.entities[1].scripts[1]
 
         return this
     end
@@ -39,37 +31,13 @@ end
 function Base.getproperty(this::PlayerMovement, s::Symbol)
     if s == :initialize
         function()
-            #println("PlayerMovement initialize")
-            event = @event begin
-                #this.jump()
-            end
             MAIN.scene.camera.target = this.parent.getTransform()
             this.animator = this.parent.getAnimator()
             this.animator.currentAnimation = this.animator.animations[1]
             this.animator.currentAnimation.animatedFPS = 0
-
-            Firebase.realdb_init("https://multiplayer-demo-2f287-default-rtdb.firebaseio.com")
-            Firebase.set_webapikey("AIzaSyCxuzQNfmIMijosSYn8UWfQGOrQYARJ4iE")
-            this.user = Firebase.firebase_signinanon()
-            initialPlayerState = Dict("id" => this.user["localId"], "name" => "toto", "direction" => "right", "color" => "blue", "x" => 3, "y" => 3, "coins" => 0)
-            this.playerId = Firebase.realdb_postRealTime("/players/$(this.user["localId"])",initialPlayerState, this.user["idToken"])["name"]
         end
     elseif s == :update
         function(deltaTime)
-            if this.test != C_NULL
-                try
-                    print("")
-                    #wait(this.test)
-                catch e
-                    this.test = C_NULL
-                    println(e)
-                    Base.show_backtrace(stderr, catch_backtrace())
-                end
-            end
-            if this.roomState != C_NULL
-                println(this.roomState)
-                this.roomState = C_NULL
-            end
 
             this.canMove = true
             x = 0
@@ -87,29 +55,7 @@ function Base.getproperty(this::PlayerMovement, s::Symbol)
 
                 this.parent.getRigidbody().grounded = false
                 y = -5.0
-                user = this.user
-                # @async begin
-                #     #print("\$(this.user["idToken"])\n")
-                #     try
-                #         print("test")
-                #         Firebase.realdb_getRealTime("/players", this.user["idToken"])
-                #     catch e
-                #         print(e)
-                #         Base.show_backtrace(stderr, catch_backtrace())
-                #     end
 
-                # end
-                this.test = Threads.@spawn this.get()
-                # this.test = @async begin 
-                #         Firebase.realdb_getRealTime("/players", this.user["idToken"])
-                #         print("hi")
-                # end
-                # @async begin
-                #     print("hi")
-                #     initialPlayerState = Dict("id" => this.user["localId"], "name" => "toto", "direction" => "right", "color" => "blue", "x" => 3, "y" => 3, "coins" => 0)
-                #     test = Firebase.realdb_postRealTime("/players/$(this.user["localId"])",initialPlayerState, this.user["idToken"])["name"]
-                # end
-                # #println(this.test)
             end
             if input.getButtonHeldDown("A") && this.canMove
                 if input.getButtonPressed("A")
@@ -163,16 +109,6 @@ function Base.getproperty(this::PlayerMovement, s::Symbol)
                 if collision.tag == "coin"
                 end
             end
-        end
-    elseif s == :get
-        function ()
-            res = nothing
-            try
-                res = Firebase.realdb_getRealTime("/players", this.user["idToken"])
-            catch e
-                print(e)
-            end
-            this.roomState = res
         end
     else
         try
