e147862fa3620c88a8a05ff01f71590465773eb6
Kyle Conel <kyjordev@gmail.com>
2023-11-19 11:13:50 -0500

    Updated file naming so project can be built properly

--- Platformer/src/Platformer.jl
+++ Platformer/src/Platformer.jl
@@ -1,4 +1,4 @@
-module Entry 
+module Platformer 
     using JulGame
     using JulGame.Math
     using JulGame.SceneBuilderModule
@@ -7,11 +7,11 @@ module Entry
         dir = @__DIR__
         #dir = pwd()
         scene = Scene(joinpath(dir, ".."), "scene.json")
-        main = scene.init(false, Vector2(576, 576),Vector2(576, 576), 2.0)
+        main = scene.init(false, Vector2(576, 576),Vector2(576, 576), 1.0)
         return main
     end
 
     julia_main() = run()
 end
 # Uncommented to allow for direct execution of this file. If you want to build this project with PackageCompiler, comment the line below
-Entry.run()
+Platformer.run()
