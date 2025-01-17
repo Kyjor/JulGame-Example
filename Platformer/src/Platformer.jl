232633332bd82a8855c5741b0341949426ad68af
Kyle Conel <kyjordev@gmail.com>
2025-01-17 06:15:45 -0500

    fix for 0.1.0

--- Platformer/src/Platformer.jl
+++ Platformer/src/Platformer.jl
@@ -1,9 +1,10 @@
 module Platformer 
     using JulGame.SceneBuilderModule: Scene, load_and_prepare_scene
-    using JulGame: Main
+    using JulGame
     function run()
+        JulGame.MAIN = JulGame.Main()
         scene = Scene("scene.json")
-        load_and_prepare_scene(Main();this=scene)
+        load_and_prepare_scene(;this=scene)
     end
 
     julia_main() = run()
