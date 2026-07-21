c6473763959b6e4660df3d1ad21c5f05b380c99d
Kyle Conel <kyjordev@gmail.com>
2026-07-21 14:43:40 -0400

    Run

--- Platformer/src/Run.jl
+++ Platformer/src/Run.jl
@@ -1,5 +1,13 @@
-include("Platformer.jl")
+proj = Base.JLOptions().project
+proj_flag = proj == C_NULL ? "" : unsafe_string(proj)
+if isempty(proj_flag)
+    println("No --project flag (default/global env)")
+    include("Platformer.jl")
+    using .Platformer
+else
+    println("Using --project=$proj_flag")
+    using Platformer
+end
 
-using .Platformer
 
 Platformer.julia_main()
